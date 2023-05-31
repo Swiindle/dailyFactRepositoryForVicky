427f43511485852bf586bbdd8cdb2bede5a18705
18markl <m.k.lim@lancaster.ac.uk>
2023-05-31 22:41:41 +0800

    Added a print function

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
-# Happy 24th Birthday Vicky!
-I have prepared for you a repository of _almost every_ Daily fact I've sent to you!
+# Happy 24th Birthday Vicky! 🎂
+Wow you're so old now! 
 
-I thought that you might appreciate this half-joke half-thoughtful repository.
+I wanted to make your birthday a little extra special, so I've collected _almost every_ Daily fact I have sent to you!
 
-The catch? Well you're going to need a C compiler :)
+I thought that you might appreciate it since you like daily facts so much.
 
-And also C is quite a low level language... please *read the code* to make sure I'm not hacking you!
+This thing _doesn't_ generate new daily facts though, so you're going to need to keep talking to me for those!
+
+BTW, please *read the code* to make sure I'm not hacking you :)
 
 # Installation / Compilation
-```
- $ gcc filename.c –o dailyfactrepository
+If you have never used GitHub before, first click on the green 'code' button, then click on the 'Download ZIP' button.
 
+After downloading, unzip the file. Then proceed to compile the .c file.
+
+I'm sure you know how to compile a .c file... right?
+```
+gcc dailyfactrepository.c –o dailyfactrepository
+```
+and then...
+
+```
  ./dailyfactrepository
 ```
--- dailyfactrepository.c
+++ dailyfactrepository.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-* This function will encrypt a string
+* This function will encrypt a string, probably won't be used in live version
 */
 char * encryptString(char str[], int arrSize)
 {
@@ -37,14 +38,21 @@ char * decryptString(char str[], int arrSize)
 /**
 * This function will print string into a nice, readable format
 */
-void printEncryptString(char str[], int arrSize)
+void printString(char str[], int arrSize)
 {
-    int i, j = 0;
-    printf("%s", str);
+    int i = 0, j = 0, textLength = 110;
+    system("clear"); // todo: change to "cls" for windows
+    while(i < textLength)
+    {
+        printf("#");
+        i++;
+    }
+    i = 0;
+    printf("\n\n\n");
     while (i < arrSize)
     {
         printf("%c",str[i]);
-        if(j == 40)
+        if(j > textLength && str[i] == ' ')
         {
             printf("\n");
             j = 0;
@@ -52,14 +60,24 @@ void printEncryptString(char str[], int arrSize)
         i++;
         j++;
     }
+    printf("\n\n\n");
+    i = 0;
+    while(i < textLength)
+    {
+        printf("#");
+        i++;
+    }
+    printf("\n");
 }
 
 int main()
 {
-    char a[] = "Even if the variable is uninitialized,\nthere should be no way printf reaches a point where it could print a minus sign when the format specifier was";
+    char a[] = "Even if the variable is uninitialized, there should be no way printf reaches a point where it could print a minus sign when the format specifier was";
     int a_size = (int) sizeof(a)/sizeof(a[0]);
-    printf("Round 1: %s\n",a);
-    printf("Round 2: %s\n",encryptString(a,a_size));
-    printf("Round 3: %s\n",decryptString(a,a_size));
+
+    //printf("Round 1: %s\n",a);
+    //printf("Round 2: %s\n",encryptString(a,a_size));
+    //printf("Round 3: %s\n",decryptString(a,a_size));
+    printString(a,a_size);
     return 0;
 }
