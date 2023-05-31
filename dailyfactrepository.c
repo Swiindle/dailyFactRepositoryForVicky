2978fa810bb74a1fcd805138d3fb9bd4cc51dcf3
18markl <m.k.lim@lancaster.ac.uk>
2023-05-31 17:23:59 +0800

    added encrypt and decrypt function. Functions now work with Strings. Added a print function

--- dailyfactrepository.c
+++ dailyfactrepository.c
@@ -1,31 +1,65 @@
 #include <stdio.h>
 
-#define STRING_MAX = 100;
-
 /**
- * This function will encrypt a string
+* This function will encrypt a string
 */
-char encryptChar(char *chrPtr)
+char * encryptString(char str[], int arrSize)
 {
-    if(*chrPtr < 36)
-        return *chrPtr;
-    else
-        return *chrPtr = *chrPtr - 3;
+    char a;
+    for(int i = 0 ; i < arrSize ; i++)
+    {
+        char a = str[i];
+        if(a < 36)
+            str[i] = a;
+        else
+            str[i] = str[i] - 3;
+    }
+    return str;
 }
 
 /**
- * This function will print string into a nice, readable format
+* This function will decrypt a string
 */
-int printDecrypt()
+char * decryptString(char str[], int arrSize)
 {
-    printf("************");
-    printf("************");
+    char a;
+    for(int i = 0 ; i < arrSize ; i++)
+    {
+        char a = str[i];
+        if(a < 36)
+            str[i] = a;
+        else
+            str[i] = str[i] + 3;
+    }
+    return str;
+}
+
+/**
+* This function will print string into a nice, readable format
+*/
+void printEncryptString(char str[], int arrSize)
+{
+    int i, j = 0;
+    printf("%s", str);
+    while (i < arrSize)
+    {
+        printf("%c",str[i]);
+        if(j == 40)
+        {
+            printf("\n");
+            j = 0;
+        }
+        i++;
+        j++;
+    }
 }
 
 int main()
 {
-    char a = '#';
-    printf("Round 1: %c\n",a);
-    printf("Round 2: %c\n",encryptChar(&a));
+    char a[] = "Even if the variable is uninitialized,\nthere should be no way printf reaches a point where it could print a minus sign when the format specifier was";
+    int a_size = (int) sizeof(a)/sizeof(a[0]);
+    printf("Round 1: %s\n",a);
+    printf("Round 2: %s\n",encryptString(a,a_size));
+    printf("Round 3: %s\n",decryptString(a,a_size));
     return 0;
 }
