19801c3210d5d551118ed9cd0adbdbd1d16d9250
18markl <m.k.lim@lancaster.ac.uk>
2023-06-03 12:41:31 +0800

    Added some LinkedList functions

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Happy 24th Birthday Vicky! 🎂
 Wow you're so old now! 
 
-I wanted to make your birthday a little extra special, so I've collected _almost every_ Daily fact I have sent to you!
+This year, I wanted to make your birthday a little extra special, so in this C program, I have collected _almost every_ Daily fact I have _ever_ sent to you!
 
-I thought that you might appreciate it since you like daily facts so much.
+I thought that you might appreciate it since it seems you like daily facts so much.
 
-This thing _doesn't_ generate new daily facts though, so you're going to need to keep talking to me for those!
+I do want to note that this program _doesn't_ generate new daily facts. You're going to need to keep talking to me for those!
 
-BTW, please *read the code* to make sure I'm not hacking you :)
+BTW, please *read the code*, there might be, idk some cool code for you in there.
 
 # Installation / Compilation
-If you have never used GitHub before, first click on the green 'code' button, then click on the 'Download ZIP' button.
+If you have never used GitHub before, first click on the green '<> Code' button, then click on the 'Download ZIP' button.
 
 After downloading, unzip the file. Then proceed to compile the .c file.
 
@@ -23,3 +23,8 @@ and then...
 ```
  ./dailyfactrepository
 ```
+
+# Potential bug
+There is potential for the program to encounter a bug on windows... (I programed on Mac).
+
+Let me know if there any problems and I'll push the fix ASAP.
--- dailyfactrepository.c
+++ dailyfactrepository.c
@@ -1,5 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// LinkedList data structure
+struct fact {
+    int size;
+    char *factData;
+    struct fact *next;
+};
+
+/**
+* This function will print a fact into a nice, readable format
+*/
+void printFact(char *str, int arrSize)
+{
+    int i = 0, j = 0, textLength = 110;
+    //system("clear"); // todo: change to "cls" for windows
+    while(i < textLength)
+    {
+        printf("#");
+        i++;
+    }
+    i = 0;
+    printf("\n\n\n");
+    while (i < arrSize)
+    {
+        printf("%c",str[i]);
+        if(j > textLength && str[i] == ' ')
+        {
+            printf("\n");
+            j = 0;
+        }
+        i++;
+        j++;
+    }
+    printf("\n\n\n");
+    i = 0;
+    while(i < textLength)
+    {
+        printf("#");
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+* This function will add a new fact to the LinkedList
+*/
+void linkedlistAdd(struct fact* head, char *str)
+{
+    struct fact *curr = head;
+    struct fact *new = (struct fact*) malloc(sizeof(struct fact));
+    int strSize = strlen(str);
+    new->size = strSize;
+    new->factData = str;
+    new->next = NULL;
+    while(curr->next != NULL)
+        curr = curr->next;
+    curr->next = new;
+}
+
+/**
+ * This function counts the number of items within the LinkedList
+*/
+int linkedlistCount(struct fact* head)
+{
+    int i = 0;
+    struct fact *curr = head;
+    while(curr->next != NULL)
+    {
+        curr = curr->next;
+        i++;
+    }
+    i++;
+    return i;
+}
+
+/*
+* Prints the content of an entire Linkedlist
+*/
+void printlinkedList(struct fact* head)
+{
+    struct fact *curr = head;
+    while(curr->next != NULL)
+    {
+        printFact(curr->factData,curr->size);
+        curr = curr->next;
+    }
+    printFact(curr->factData,curr->size);
+}
 
 /**
 * This function will encrypt a string, probably won't be used in live version
@@ -35,49 +124,19 @@ char * decryptString(char str[], int arrSize)
     return str;
 }
 
-/**
-* This function will print string into a nice, readable format
-*/
-void printString(char str[], int arrSize)
-{
-    int i = 0, j = 0, textLength = 110;
-    system("clear"); // todo: change to "cls" for windows
-    while(i < textLength)
-    {
-        printf("#");
-        i++;
-    }
-    i = 0;
-    printf("\n\n\n");
-    while (i < arrSize)
-    {
-        printf("%c",str[i]);
-        if(j > textLength && str[i] == ' ')
-        {
-            printf("\n");
-            j = 0;
-        }
-        i++;
-        j++;
-    }
-    printf("\n\n\n");
-    i = 0;
-    while(i < textLength)
-    {
-        printf("#");
-        i++;
-    }
-    printf("\n");
-}
-
 int main()
 {
-    char a[] = "Even if the variable is uninitialized, there should be no way printf reaches a point where it could print a minus sign when the format specifier was";
-    int a_size = (int) sizeof(a)/sizeof(a[0]);
-
-    //printf("Round 1: %s\n",a);
-    //printf("Round 2: %s\n",encryptString(a,a_size));
-    //printf("Round 3: %s\n",decryptString(a,a_size));
-    printString(a,a_size);
+    char *a = "Even if the variable is uninitialized, there should be no way printf reaches a point where it could print a minus sign when the format specifier was";
+    struct fact head = {.size = strlen(a), .factData = a, .next = NULL};
+    struct fact *head_ptr = &head;
+    char *b = "Test 123 hello world!";
+    char *c = "My name is mark!";
+    char *d = "wow what an interesting fact!";
+   
+    linkedlistAdd(head_ptr,b);
+    linkedlistAdd(head_ptr,c);
+    linkedlistAdd(head_ptr,d);
+    printlinkedList(head_ptr);
+    printf("Linkedlist count: %d\n",linkedlistCount(head_ptr));
     return 0;
 }
