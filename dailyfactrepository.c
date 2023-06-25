f2ba7d6335cc793dfa4641059f23d870d189f285
18markl <m.k.lim@lancaster.ac.uk>
2023-06-25 23:47:14 +0800

    small improvements to User Experience, changed to Windows

--- dailyfactrepository.c
+++ dailyfactrepository.c
@@ -128,22 +128,22 @@ void printlinkedList(struct fact* head)
 {
     struct fact *curr = head;
     char input[50];
-    system("clear"); // to-do cls for windows
+    system("cls"); // to-do cls for windows
 
     while (curr != NULL)
     {
         printFact(curr);
         curr = curr->next;
-        printf("> ");
+        printf("Press enter for next fact: ");
         fgets(input, sizeof(input), stdin);
-        system("clear"); // to-do cls for windows
+        system("cls"); // to-dgito cls for windows
     }
 }
 
 int main()
 {
     // HEAD INITIALIZATION
-    char a[] = "Daily Fact Repository 2020 - 2023\nBuilt by Mark, just for Vicky!\nInput and press enter anything to navigate to the next fact.";
+    char a[] = "Daily Fact Repository 2020 - 2023\nBuilt by Mark, for Vicky's 24th birthday!\nPress enter to navigate to the next fact.";
     struct fact head = {.id = 0, .size = strlen(a), .factData = a, .next = NULL};
     struct fact *head_ptr = &head;
 
@@ -154,7 +154,7 @@ int main()
     char fact4[] = "M^sils^ fp k^jba ^cqbo >kk^ M^sils^ qeb ?^iibofk^ cliiltfkd ebo qlro fk >rpqo^if^ # Kbt Wb^i^ka+";
     char fact5[] = "Qeb .6.3 Wlkfkd Obplirqflk af`q^qbp qe^q qeb q^iibo qeb _rfiafkd fk Kbt Vloh) qeb pj^iibo b^`e pr_pbnrbkq cillo tfii _b+ Qeb obplirqflk) ^iqelrde mo^`qf`^i) fp ^ ob^plk tev j^kv _rfiafkdp ^olrka qeb tloia illh qeb t^v qebv al!";
     char fact6[] = "Ilkd*cfkkba bbip `^k pljbqfjbp _b clrka ptfjjfkd fk qeb mlkap lc Pvakbv$p Olv^i ?lq^kf` D^oabk+ Qebpb bbip ^ob lkiv qbjmlo^ofiv ifsfkd fk Pvakbv) qebv tfii jfdo^qb _^`h eljb ql _obba ^ka afb ^cqbo {1- vb^op+";
-    char fact7[] = "Qeb ^``bkq J^oh kloj^iiv rpbp fp c^hb+ Eb e^p ^ jlob ^pf^k plrkafkd ^``bkq qe^q lkiv ^mmb^op ql c^jfiv jbj_bop+";
+    char fact7[] = "Qeb ^``bkq J^oh kloj^iiv rpbp fp+++ c^hb+ Eb e^p ^ jlob ^pf^k plrkafkd ^``bkq qe^q lkiv ^mmb^op ql c^jfiv jbj_bop+";
     char fact8[] = "J^oh$p ilsb lc c^`qp `ljbp colj Do^ab 1) tebk efp qb^`ebo ^phba efj ql obpb^o`e ^ka mobpbkq c^`qp ql qeb obpq lc efp `i^pp+";
     char fact9[] = "Qtl lc Pvakbv$p abcrk`q jlklo^fi pbosf`b `^oof^dbp `^k _b clrka fk qeb Dlldib lccf`bp+";
     char fact10[] = "Mlkqb @fqv fp qeb k^jb lc ^ lk`b iruroflrp _rfiafkd clo qeb of`e ^ka tb^iqev fk Plrqe >cof`^+ >cqbo qeb >m^oqebfa fq _b`^jb ^ pirj+";
@@ -180,5 +180,6 @@ int main()
 
     //MAIN LOGIC
     printlinkedList(head_ptr);
+    printf("Program finished.\nTotal facts: %d\n",linkedlistCount(head_ptr) - 1);
     return 0;
 }
